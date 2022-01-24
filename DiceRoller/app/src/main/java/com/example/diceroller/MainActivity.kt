d257646de3eb526d9b48669640cb524f0f37c6ee
Matthew-Place <matt.1504@hotmail.co.uk>
2022-01-24 18:05:40 +0000

    Count button and function added

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,17 +6,30 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val resultTextView: TextView = findViewById(R.id.result_text_view)
+
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDice(resultTextView) }
+
+        val countButton: Button = findViewById(R.id.count_button)
+        countButton.setOnClickListener { countUp(resultTextView) }
     }
 
-    private fun rollDice() {
-//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text_view)
-        resultText.text = randomInt.toString()
+    private fun rollDice(resultTextView: TextView) {
+//      Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val randomInt: Int = (1..6).random()
+        resultTextView.text = randomInt.toString()
+    }
+
+    private fun countUp(resultTextView: TextView) {
+        var resultNum: Int = resultTextView.text.toString().toInt()
+        resultNum += 1
+        if (resultNum == 7) resultNum = 1 else resultNum %= 7
+        resultTextView.text = resultNum.toString()
     }
 }
