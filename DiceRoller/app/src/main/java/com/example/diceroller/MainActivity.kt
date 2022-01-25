4362314d3a721f3168700f5904ee859aad94e175
Matthew-Place <matt.1504@hotmail.co.uk>
2022-01-25 16:39:45 +0000

    2 sided dice is referred to as a coin, text and result is modified to fit.

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     private var diceSides: Int = 6
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,23 +15,34 @@ class MainActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById(R.id.result_text_view)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice(resultTextView) }
+        rollButton.setOnClickListener {
+            if (diceSides == 2) flipCoin(resultTextView) else rollDice(
+                resultTextView
+            )
+        }
 
         val countButton: Button = findViewById(R.id.count_button)
         countButton.setOnClickListener { countUp(resultTextView) }
 
-        headerTextUpdate()
+        update(resultTextView, true)
         val increaseButton: Button = findViewById(R.id.increase_button)
-        increaseButton.setOnClickListener { changeDice(true) }
+        increaseButton.setOnClickListener { changeDice(resultTextView, true) }
 
         val decreaseButton: Button = findViewById(R.id.decrease_button)
-        decreaseButton.setOnClickListener { changeDice(false) }
+        decreaseButton.setOnClickListener { changeDice(resultTextView, false) }
     }
 
     private fun rollDice(resultTextView: TextView) {
 //      Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         val randomInt: Int = (1..diceSides).random()
         resultTextView.text = randomInt.toString()
+    }
+
+    private fun flipCoin(resultTextView: TextView) {
+//      Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val randomInt: Int = (1..diceSides).random()
+        if (randomInt == 1) resultTextView.text =
+            getString(R.string.heads_text) else resultTextView.text = getString(R.string.tails_text)
     }
 
     private fun countUp(resultTextView: TextView) {
@@ -42,14 +52,39 @@ class MainActivity : AppCompatActivity() {
         resultTextView.text = resultNum.toString()
     }
 
-    private fun changeDice(increase: Boolean) {
-        if (increase) this.diceSides += 1 else if (diceSides > 3) this.diceSides -= 1
-        headerTextUpdate()
+    private fun changeDice(resultTextView: TextView, increase: Boolean) {
+        if (increase) this.diceSides += 1 else if (diceSides > 2) this.diceSides -= 1
+        update(resultTextView, increase)
     }
 
-    private fun headerTextUpdate() {
-        val headerNewText: String = getString(R.string.header_text, diceSides)
+    private fun update(resultTextView: TextView, increase: Boolean) {
         val headerTextView: TextView = findViewById(R.id.header_text_view)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val countButton: Button = findViewById(R.id.count_button)
+
+        var headerNewText: String = getString(R.string.header_roll_text, diceSides)
+        if (diceSides == 2 && !increase) {
+            headerNewText = getString(R.string.header_flip_text)
+            changeToCoin(rollButton, countButton)
+            flipCoin(resultTextView)
+        } else if (diceSides == 3 && increase) {
+            changeToDice(rollButton, countButton)
+            rollDice(resultTextView)
+        } else rollDice(resultTextView)
         headerTextView.text = headerNewText
+    }
+
+    private fun changeToCoin(rollButton: Button, countButton: Button) {
+        rollButton.text = getString(R.string.flip_button_text)
+        countButton.isEnabled = false
+        countButton.isClickable = false
+        countButton.alpha = 0F
+    }
+
+    private fun changeToDice(rollButton: Button, countButton: Button) {
+        rollButton.text = getString(R.string.roll_button_text)
+        countButton.isEnabled = true
+        countButton.isClickable = true
+        countButton.alpha = 1F
     }
 }
